70310334078cc39f1096556476ef2ad20b54109d
Matt McCoy <mdmccoy@gmail.com>
2019-05-04 20:15:49 -0400

    master - Fix file path

--- geocommit/geocommit.rb
+++ geocommit/geocommit.rb
@@ -1,9 +1,12 @@
 require 'httparty'
 
-geo_info = HTTParty.get('http://ip-api.com/json').parsed_response
+url = 'http://ip-api.com/json'
+file = '~/source/scripts/geocommit/locations.txt'
+
+geo_info = HTTParty.get(url).parsed_response
 
 geo_info['datetime'] = Time.now.utc.to_s
 
 puts geo_info
 
-File.open('locations.txt', 'a+') { |f| f.puts "#{geo_info}," }
+File.open(file, 'a+') { |f| f.puts "#{geo_info}," }
